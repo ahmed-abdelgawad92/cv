c17921a0f9b87734963632109901425bb84d22cd
Ahmed Abdelgawad <a.abdelgawad@gmail.com>
2018-09-06 16:22:27 +0200

    change logo by scroll, commenting links and scripts of bstrap jq

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
+  // Das ist füt mehrere img vorzuladen
+  // $.fn.preload = function() {
+  //   this.each(function(){
+  //       $('<img />')[0].src = this;
+  //   });
+  // }
+  // $(['img1.jpg','img2.jpg','img3.jpg']).preload();
+  $('<img src="img/logo_icon.png"/>');
   $(window).scroll(function(){
     if($(this).scrollTop()>100){
       $('#logo').addClass('small-logo');
+      $('#logo').attr('src','img/logo_icon.png');
     }else{
       $('#logo').removeClass('small-logo');
-
+      $('#logo').attr('src','img/logo.png');
     }
   });
 });
